7414f68653f448839dae74be9ef4522b4ad51e29
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-12-05 17:29:17 +0900

    usersテーブルのカラム設定

--- db/migrate/20201205081519_devise_create_users.rb
+++ db/migrate/20201205081519_devise_create_users.rb
@@ -4,8 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :nickname,           null: false
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.integer :prefecture_id,      null: false
+      t.string  :city
+      t.integer :admin_id,           default: ""
 
       ## Recoverable
       t.string   :reset_password_token
